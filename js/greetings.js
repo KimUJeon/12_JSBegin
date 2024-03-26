4d2a5a2df48a9cd6eb42b2a13e7fe83593a74472
KimUJeon <kinfe_411@naver.com>
2024-03-26 16:38:02 +0900

    5.0 Intervals Start

---
+++ js/greetings.js
@@ -0,0 +1,35 @@
+﻿// querySelector => class, id 모두 검색이 가능해 id 앞에 #을 붙여야함, byid는 id만 검색
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings();
+}
+
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function handleLinkClick(event){
+    event.preventDefault;
+    console.log(event);
+    alert("clicked");
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings();
+}
