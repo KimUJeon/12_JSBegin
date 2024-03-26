67d2bca73a2b942dc33e6432e93a04dee6f63358
KimUJeon <kinfe_411@naver.com>
2024-03-26 17:46:10 +0900

    6.1 Background

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,15 @@
-﻿
+﻿const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    const second = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hour}:${minute}:${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+// setInterval 과 유사한 기능임- 한번만 사용
+// setTimeout(sayHello, 5000);
